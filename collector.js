61efacae702d91ca219663848cc1a001e30ad811
NabeelRehman1 <114175801+NabeelRehman1@users.noreply.github.com>
2023-02-08 01:37:42 +0000

    Add files via upload

---
+++ collector.js
@@ -0,0 +1,26 @@
+// your code 
+// require the coins array
+
+function coinCollector(coins) {
+  const onlyCoins = [];
+
+  const desired = [
+    'quarter',
+    'dime',
+    'nickel',
+    'penny'
+  ];
+
+  for (let i = 0; i < coins.length; i++) {
+    const coin = coins[i];
+    if (desired.includes(coin)) {
+      onlyCoins.push(coins[i]);
+    }
+  }
+  return onlyCoins;
+}
+
+coinCollector(coins);
+
+// your code
+// export the coinCollector function
